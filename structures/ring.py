57cc3eb8509a8797f19aaeccea18781824c91c98
Pablo <pasanz10@ucm.es>
2020-10-28 19:20:49 +0100

    Using dtype for ring type and allowed LaTeX repr

--- structures/ring.py
+++ structures/ring.py
@@ -15,28 +15,28 @@ class Ring(abc.ABC):
     extended with subclasses.
     """
 
-    def __init__(self, element_type):
+    def __init__(self, dtype):
         """
-        :param element_type: type - type of the elements contained in this ring
+        :param dtype: type - type of the elements contained in this ring
         """
-        self._element_type = element_type
+        self._dtype = dtype
 
     @property
-    def element_type(self):
+    def dtype(self):
         """
         Type of the elements contained in this ring.
         :return: type - type of the elements contained in this ring
         """
-        return self._element_type
+        return self._dtype
 
     def add(self, a, b):
         """
         Additive operation. It must be associative (a + (b + c) = (a + b) + c), commutative (a + b = b + a),
         there must be an identity element 0 (property self.zero) such that a + 0 = a for all a and every element must
         have an opposite (for all a there exists an additive inverse element b such that a + b = 0).
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: self.element_type - the sum a + b
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: self.dtype - the sum a + b
         """
         raise NotImplementedError
 
@@ -45,9 +45,9 @@ class Ring(abc.ABC):
         Multiplicative operation. It must be associative (a * (b * c) = (a * b) * c) and distributive wrt the sum
         (a * (b + c) = a * b + a * c , (a + b) * c = a * c + b * c). If this operation is also commutative the ring
         is called a commutative ring.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: self.element_type - the product a * b
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: self.dtype - the product a * b
         """
         raise NotImplementedError
 
@@ -55,15 +55,15 @@ class Ring(abc.ABC):
     def zero(self):
         """
         Identity element for the sum.
-        :return: self.element_type - identity element for the sum 0 such that 0 + a = a for all a
+        :return: self.dtype - identity element for the sum 0 such that 0 + a = a for all a
         """
         raise NotImplementedError
 
     def negate(self, a):
         """
         Returns the opposite element for the sum of the given element a. That is, en element b such that a + b = 0.
-        :param a: self.element_type - element for which return the opposite
-        :return: self.element_type - a's opposite element: b = -a, such that a + b = 0
+        :param a: self.dtype - element for which return the opposite
+        :return: self.dtype - a's opposite element: b = -a, such that a + b = 0
         """
         raise NotImplementedError
 
@@ -71,9 +71,9 @@ class Ring(abc.ABC):
         """
         Returns the result of subtracting b to a. The subtraction operation is defined as a + (-b), where -b is the
         opposite of b. Subclasses may override this method to make it more efficient.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: self.element_type - the subtraction a - b
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: self.dtype - the subtraction a - b
         """
         self._check_params(a, b)
         return self.add(a, self.negate(b))
@@ -81,9 +81,9 @@ class Ring(abc.ABC):
     def times(self, a, n):
         """
         Returns the result of adding n times the element a to itself, that is, the sum from 1 to n of a.
-        :param a: self.element_type - element to be added
+        :param a: self.dtype - element to be added
         :param n: int - number of times to add the element (n >= 0)
-        :return: self.element_type - sum(1, n; a) = na
+        :return: self.dtype - sum(1, n; a) = na
         """
         self._check_params(a)
         return reduce(lambda x, y: self.sum(x, y), (a for _ in range(n)), self.zero)
@@ -91,9 +91,9 @@ class Ring(abc.ABC):
     def pow(self, a, n):
         """
         Returns the result of multiplying n times a to itself, that is, the product from 1 to n of a.
-        :param a: self.element_type - element to be multiplied
+        :param a: self.dtype - element to be multiplied
         :param n: int - number of times to multiply the element (n > 0)
-        :return: self.element_type - product(1, n; a) = a^n
+        :return: self.dtype - product(1, n; a) = a^n
         """
         self._check_params(a)
         return reduce(lambda x, y: self.mul(x, y), (a for _ in range(n)))
@@ -101,7 +101,7 @@ class Ring(abc.ABC):
     def is_idempotent(self, a):
         """
         Determines if the given element is idempotent, that is, a^2 = a.
-        :param a: self.element_type - element to be checked
+        :param a: self.dtype - element to be checked
         :return: bool - whether a is idempotent or not
         """
         return self.mul(a, a) == a
@@ -115,10 +115,24 @@ class Ring(abc.ABC):
         """
         raise NotImplementedError
 
+    def __latex__(self):
+        """
+        Returns the LaTeX representation of this class. This method is here only so subclasses don't forget to add the
+        LaTeX representation.
+        :return: str - LaTeX representation of this class
+        """
+        # If there's no such LaTeX representation just return None (or leave it unimplemented)
+        raise NotImplementedError
+
+    def _repr_latex_(self):
+        # Do not override this method
+        return self.__latex__()
+
     def _check_params(self, *args):
         for a in args:
-            if not isinstance(a, self._element_type):
-                raise ValueError(f"expected {self._element_type} but got {type(a)}: {a}")
+            # FIXME it may not work well with numpy (np.int32/64, np.uint32/64 or np.integer are not the same as int)
+            if not isinstance(a, self._dtype):
+                raise ValueError(f"expected {self._dtype} but got {type(a)}: {a}")
 
 
 class UnitaryRing(Ring, abc.ABC):
@@ -132,7 +146,7 @@ class UnitaryRing(Ring, abc.ABC):
     def one(self):
         """
         Identity element for the product.
-        :return: self.element_type - identity element for the product 1 such that 1 * a = a for all a
+        :return: self.dtype - identity element for the product 1 such that 1 * a = a for all a
         """
         raise NotImplementedError
 
@@ -140,7 +154,7 @@ class UnitaryRing(Ring, abc.ABC):
         """
         Determines whether a is an unit of this ring or not, that is, if there exits an inverse element of a for the
         product (some b such that a * b = 1).
-        :param a: self.element_type - element to be checked
+        :param a: self.dtype - element to be checked
         :return: bool - True if a is an unit, False otherwise
         """
         raise NotImplementedError
@@ -149,8 +163,8 @@ class UnitaryRing(Ring, abc.ABC):
         """
         Returns the inverse element of a for the product, assuming there exits one. That is, the element b such that
         a * b = 1.
-        :param a: self.element_type - element for which calculate its inverse
-        :return: self.element_type - the inverse element of a: b = a^-1, such that a * b = 1
+        :param a: self.dtype - element for which calculate its inverse
+        :return: self.dtype - the inverse element of a: b = a^-1, such that a * b = 1
         :raise: ValueError - if a is not an unit
         """
         raise NotImplementedError
@@ -159,9 +173,9 @@ class UnitaryRing(Ring, abc.ABC):
         """
         Returns the power a^n. It differs from the original method (in Ring) in the way that this method does allow
         n <= 0. If n = 0 self.one will be returned and if n < 0 the result will be (a^-1)^n as long as a is an unit.
-        :param a: self.element_type - element to be multiplied
+        :param a: self.dtype - element to be multiplied
         :param n: int - number of times to multiply the element
-        :return: self.element_type - product(1, n; a) = a^n
+        :return: self.dtype - product(1, n; a) = a^n
         :raise: ValueError - if n < 0 and a is not an unit
         """
         self._check_params(a)
@@ -220,7 +234,7 @@ class UFD(Domain, abc.ABC):
         """
         Determines whether the element p is prime or not. A prime element is a non zero element for which the principal
         ideal (p) generated by p is a nonzero prime ideal.
-        :param p: self.element_type - element to be checked
+        :param p: self.dtype - element to be checked
         :return: bool - True if p is prime, False otherwise
         """
         raise NotImplementedError
@@ -229,8 +243,8 @@ class UFD(Domain, abc.ABC):
         """
         Returns a list containing the unique prime factorization of a. If an element has multiplicity n,
         it will appear n times, and 1 won't be in the list.
-        :param a: self.element_type - element to be factorized
-        :return: [self.element_type] - list of prime elements that decompose a
+        :param a: self.dtype - element to be factorized
+        :return: [self.dtype] - list of prime elements that decompose a
         """
         # TODO break RSA :)
         raise NotImplementedError
@@ -249,7 +263,7 @@ class EuclideanDomain(UFD, abc.ABC):
         - if b != 0 and there exist q, r such that a = b*q + r, then r = 0 or v(r) < v(b)
         - if b != 0, v(a) <= v(a*b)
         This function will vary depending on the ring.
-        :param a: self.element_type - element for which compute the Euclidean function
+        :param a: self.dtype - element for which compute the Euclidean function
         :return: int - result of applying the Euclidean function to a
         """
         raise NotImplementedError
@@ -257,35 +271,35 @@ class EuclideanDomain(UFD, abc.ABC):
     def quot(self, a, b):
         """
         Returns the quotient q of the division a = b * q + r.
-        :param a: self.element_type - dividend
-        :param b: self.element_type - divisor
-        :return: self.element_type - quotient of the division
+        :param a: self.dtype - dividend
+        :param b: self.dtype - divisor
+        :return: self.dtype - quotient of the division
         """
         return self.full_div(a, b)[0]
 
     def rem(self, a, b):
         """
         Returns the remainder r of the division a = b * q + r.
-        :param a: self.element_type - dividend
-        :param b: self.element_type - divisor
-        :return: self.element_type - remainder of the division
+        :param a: self.dtype - dividend
+        :param b: self.dtype - divisor
+        :return: self.dtype - remainder of the division
         """
         return self.full_div(a, b)[1]
 
     def full_div(self, a, b):
         """
         Returns both the quotient q and the remainder r of the division a = b * q + r.
-        :param a: self.element_type - dividend
-        :param b: self.element_type - divisor
-        :return: (self.element_type, self.element_type) - quotient and remainder of the division
+        :param a: self.dtype - dividend
+        :param b: self.dtype - divisor
+        :return: (self.dtype, self.dtype) - quotient and remainder of the division
         """
         raise NotImplementedError
 
     def divides(self, a, b):
         """
         Determines whether a divides b or not (a|b), that is if there exists an element c for which b = a * c.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side
         :return: bool - True if a|b, else False
         """
         return self.rem(a, b) == self.zero
@@ -293,18 +307,18 @@ class EuclideanDomain(UFD, abc.ABC):
     def gcd(self, a, b):
         """
         Returns the greatest common divisor of a and b, that is, the greatest element which divides both a and b.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: self.element_type - greatest common divisor of a and b
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: self.dtype - greatest common divisor of a and b
         """
         raise NotImplementedError
 
     def lcm(self, a, b):
         """
         Returns the least common multiple of a and b, that is, the lowest element which is divided by both a and b.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: self.element_type - least common multiple of a and b
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: self.dtype - least common multiple of a and b
         """
         # TODO return self.quot(self.mul(a, b), self.gcd(a, b)) ?
         raise NotImplementedError
@@ -313,9 +327,9 @@ class EuclideanDomain(UFD, abc.ABC):
         """
         Returns two elements that satisfy Bezout's identity, that is, two elements x and y for which
         gcd(a, b) = a * x + b * y.
-        :param a: self.element_type - left-hand-side element
-        :param b: self.element_type - right-hand-side element
-        :return: (self.element_type, self.element_type) - two elements that satisfy Bezout's identity
+        :param a: self.dtype - left-hand-side element
+        :param b: self.dtype - right-hand-side element
+        :return: (self.dtype, self.dtype) - two elements that satisfy Bezout's identity
         """
         raise NotImplementedError
 
@@ -328,9 +342,9 @@ class Field(EuclideanDomain, abc.ABC):
     def div(self, a, b):
         """
         Alias of EuclideanDomain.quot, which makes more sense in the context of a field.
-        :param a: self.element_type - dividend
-        :param b: self.element_type - divisor
-        :return: self.element_type - quotient of the division
+        :param a: self.dtype - dividend
+        :param b: self.dtype - divisor
+        :return: self.dtype - quotient of the division
         """
         if b == self.zero:
             raise ZeroDivisionError
