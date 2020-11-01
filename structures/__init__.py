227e98c4388d8da28fd11f1e32e718ad56f9da4f
Pablo <pasanz10@ucm.es>
2020-11-01 17:55:17 +0100

    Reorganized classes

---
+++ structures/__init__.py
@@ -0,0 +1,15 @@
+from .domains import Domain, IntegralDomain, UFD, EuclideanDomain
+from .fields import Field
+from .rings import Ring, UnitaryRing, CommutativeRing, DivisionRing
+
+__all__ = [
+    'Ring',
+    'UnitaryRing',
+    'CommutativeRing',
+    'DivisionRing',
+    'Domain',
+    'IntegralDomain',
+    'UFD',
+    'EuclideanDomain',
+    'Field'
+]
---
+++ structures/fields.py
@@ -0,0 +1,41 @@
+import abc
+
+from .domains import EuclideanDomain
+
+
+class Field(EuclideanDomain, abc.ABC):
+    """
+    Class representing a field, a ring for which all elements are units but 0.
+    """
+
+    def div(self, a, b):
+        """
+        Alias of EuclideanDomain.quot, which makes more sense in the context of a field.
+        :param a: self.dtype - dividend
+        :param b: self.dtype - divisor
+        :return: self.dtype - quotient of the division
+        """
+        if b == self.zero:
+            raise ZeroDivisionError
+        return self.mul(a, self.inverse(b))
+
+    def quot(self, a, b):
+        return self.div(a, b)
+
+    def rem(self, a, b):
+        return self.mod(a, b)
+
+    def full_div(self, a, b):
+        return self.div(a, b), 0
+
+    def is_unit(self, a):
+        # All elements but 0 are units
+        return a != self.zero
+
+    def is_prime(self, p):
+        # Fields have no primes
+        return False
+
+    def factor(self, a):
+        # Unit factorization is an empty product
+        return []
